d319fa06d77ca5f1cec0e93ddc46297b287d678c
Zain <m.zainhassanbaloch@gmail.com>
2024-08-16 02:29:36 +0500

    Form setup in html and JS

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,13 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
     handleContactForm();
+    handleRegistrationForm();
 });
+
+function handleRegistrationForm() {
+    const form = document.getElementById('registrationForm');
+    const fileInput = document.getElementById('business_license');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const file = fileInput.files[0];
+
+        if (file && file.type !== 'application/pdf') {
+            alert('Please upload a PDF file for Gewerbeanmeldung.');
+            fileInput.value = ''; // Clear the file input
+            return;
+        }
+
+        const formData = new FormData(form);
+        fetch('/send-registration-email/', {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+            },
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                alert('Form submitted successfully!');
+                form.reset(); // Clear the form fields
+            } else {
+                alert('Error submitting form: ' + data.message);
+                form.reset();
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again later.');
+        });
+
+    });
+}
 
 function handleContactForm() {
     const form = document.getElementById('contactForm');
     form.addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission
 
-        console.log("Form Submitted");
         const formData = new FormData(form);
         const data = {};
 
@@ -24,18 +64,18 @@ function handleContactForm() {
             },
             body: JSON.stringify(data)
         })
-        .then(response => response.json())
-        .then(data => {
-            if(data.status==='success') {
-                console.log('Email sent successfully');
-            }
-            else {
-                console.error('Error sending email:', data.message);
-            }
-            form.reset();
-        })
-        .catch(error => {
-            console.error('Cannot parse the content to json:', error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    console.log('Email sent successfully');
+                }
+                else {
+                    console.error('Error sending email:', data.message);
+                }
+                form.reset();
+            })
+            .catch(error => {
+                console.error('Cannot parse the content to json:', error);
+            });
     });
 }
